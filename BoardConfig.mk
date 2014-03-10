132a4df92990fcca7b2a60d1dcd92948fe19f02f
Hard Light <hardlightxda@gmail.com>
2014-03-10 10:30:47 +0200

    TWRP Recovery: Fix for USBMS and Brightness (2/2)
    - Create general config file for recovery
    - Fix display brightness
    - Fix mount mass storage
    
    Change-Id: I80985ea23ee45527d33580e781946d2fd3d11dec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 ifneq (eng,$(TARGET_BUILD_VARIANT))
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
@@ -15,9 +14,8 @@ TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
 TARGET_RECOVERY_FSTAB := device/lge/p500/fstab.p500
 
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
-
-P500_SPEAKER_IN_CALL_FIX := true
 
 # TWRP Recovery
 DEVICE_RESOLUTION := 320x480
